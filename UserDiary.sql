615a4b0fd514d6ed926b89614a3bc54e97f5675a
Seana36 <sean.emily11@gmail.com>
2017-04-17 19:05:23 -0400

    Count working

--- UserDiary.sql
+++ UserDiary.sql
@@ -1,17 +1,18 @@
 CREATE TABLE UserDiary (
 	userID 		INTEGER NOT NULL,
-	meal		VARCHAR(10),
+	meal		VARCHAR(10) NOT NULL,
 	NDB_NO		INTEGER NOT NULL, 
-	date		date,
+	date		date NOT NULL,
+	servings	INTEGER  NOT NULL, 
 	FOREIGN KEY (NDB_NO) REFERENCES mytable (NDB_NO)
 );
 
-INSERT INTO UserDiary(userID, meal, NDB_NO, date) VALUES (1, "breakfast", 02001, '2017-10-05');
-INSERT INTO UserDiary(userID, meal, NDB_NO, date) VALUES (1, "breakfast", 02002, '2017-10-05');
-INSERT INTO UserDiary(userID, meal, NDB_NO, date) VALUES (1, "breakfast", 02003, '2017-10-05');
-INSERT INTO UserDiary(userID, meal, NDB_NO, date) VALUES (1, "lunch", 02004, '2017-10-05');
-INSERT INTO UserDiary(userID, meal, NDB_NO, date) VALUES (1, "lunch", 02005, '2017-10-05');
-INSERT INTO UserDiary(userID, meal, NDB_NO, date) VALUES (1, "lunch", 02006, '2017-10-05');
-INSERT INTO UserDiary(userID, meal, NDB_NO, date) VALUES (1, "dinner", 02007, '2017-10-05');
-INSERT INTO UserDiary(userID, meal, NDB_NO, date) VALUES (1, "dinner", 02008, '2017-10-05');
-INSERT INTO UserDiary(userID, meal, NDB_NO, date) VALUES (1, "dinner", 02009, '2017-10-05');
+INSERT INTO UserDiary(userID, meal, NDB_NO, date, servings) VALUES (1, "breakfast", 02001, '2017-10-05', 1);
+INSERT INTO UserDiary(userID, meal, NDB_NO, date, servings) VALUES (1, "breakfast", 02002, '2017-10-05', 1);
+INSERT INTO UserDiary(userID, meal, NDB_NO, date, servings) VALUES (1, "breakfast", 02003, '2017-10-05', 1);
+INSERT INTO UserDiary(userID, meal, NDB_NO, date, servings) VALUES (1, "lunch", 02004, '2017-10-05', 1);
+INSERT INTO UserDiary(userID, meal, NDB_NO, date, servings) VALUES (1, "lunch", 02005, '2017-10-05', 1);
+INSERT INTO UserDiary(userID, meal, NDB_NO, date, servings) VALUES (1, "lunch", 02006, '2017-10-05', 1);
+INSERT INTO UserDiary(userID, meal, NDB_NO, date, servings) VALUES (1, "dinner", 02007, '2017-10-05', 1);
+INSERT INTO UserDiary(userID, meal, NDB_NO, date, servings) VALUES (1, "dinner", 02008, '2017-10-05', 1);
+INSERT INTO UserDiary(userID, meal, NDB_NO, date, servings) VALUES (1, "dinner", 02009, '2017-10-05', 1);
--- addto_UserDiary.php
+++ addto_UserDiary.php
@@ -1,30 +1,65 @@
 <?php
 session_start(); 
 include_once('dbConnect.php');
-include_once('isLoggedIn.php'); 
-
+/*include_once('isLoggedIn.php');
+ 
 $user = $_SESSION['userID']; 
 $id = intval($_POST['ID']);
-$meal = $_POST['meal'];
-echo $id; 
-echo $meal; 
-echo $id; 
-$sql = "INSERT INTO UserDiary(userID, meal, NDB_NO, date) VALUES ($user, '$meal' ,$id, '2017-10-06')";
-$sql2 = "SELECT count FROM Freq_Item WHERE userID = '$user'  AND NDB_NO = $id "; 
-$result2 = $conn->query($sql2);
+$meal = $_POST['meal'];*/
+$user = 1;
+$id = 2051;
+$meal = 'Breakfast'; 
 
-if ($conn->query($sql) === TRUE) {
+/*echo $id; 
+echo $meal; 
+echo $id; */
+$sql11 = "SELECT servings FROM UserDiary WHERE userID = $user AND meal = '$meal' and NDB_NO = $id AND date =  '2017-10-06' "; 
+
+/*$sql = "INSERT INTO UserDiary(userID, meal, NDB_NO, date) VALUES ($user, '$meal' ,$id, '2017-10-06')";*/
+$sql2 = "SELECT count FROM Freq_Item WHERE userID = '$user'  AND NDB_NO = $id "; 
+
+
+$result11 = $conn->query($sql11);
+if($result11->num_rows > 0) {
+	while($row = $result11->fetch_assoc()) {
+		$servings = intval($row['servings']) + 1; 
+		var_dump($row['servings']);
+		echo "<script> console.log($servings) </script>";
+		#$sql = "INSERT INTO UserDiary(userID, meal, NDB_NO, date, servings) VALUES ($user, '$meal' ,$id, '2017-10-06', $servings)";
+		$sql = "UPDATE UserDiary SET servings = $servings WHERE userID = $user AND NDB_NO = $id AND meal = '$meal' AND date = '2017-10-06'"; 
+		$result = $conn->query($sql);
+	}
+}
+else 
+{
+	$sql = "INSERT INTO UserDiary(userID, meal, NDB_NO, date, servings) VALUES ($user, '$meal' ,$id, '2017-10-06', 1)";
+	$result = $conn->query($sql);
+}
+
+
+
+$result2 = $conn->query($sql2);
+if ($result === TRUE) {
+	//if the item is already in Freq_item, 
+	//increment the count
 	if ($result2->num_rows > 0) {
-/*		while($row = $result->fetch_assoc()) {
-			echo "var_dump($row)"; 
-		}*/
+		while($row = $result2->fetch_assoc()) {
+			$counter = intval($row['count']) + 1; 
+			$sql3 = "UPDATE  Freq_Item SET count =  $counter WHERE userID = '$user'  AND NDB_NO = $id "; 
+			if ($conn->query($sql3) === TRUE) {
+				echo "Add successfully to your diary and updated the count";
+			}
+		}
 	}else {
+	//item is not in Freq_Item
+	//set count = 1	
 		 echo "Add successfully to your diary";
+		 $sql3 = "INSERT INTO Freq_Item(userID, NDB_NO, count) VALUES ($user, $id, 1) ";
+		 if ($conn->query($sql3) === TRUE) {
+				echo "Add successfully to your diary and updated the count to 1";
+		}
 	}
 
-
-
-   
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
